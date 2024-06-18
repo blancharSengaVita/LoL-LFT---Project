f8acedfa99eee4f70e5dd787f7929a9168382d2a
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2024-06-18 19:03:39 +0200

    Finish Awards crud

--- database/factories/DisplayedInformationFactory.php
+++ database/factories/DisplayedInformationFactory.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DisplayedInformations>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DisplayedInformation>
  */
 class DisplayedInformationsFactory extends Factory
 {
--- database/factories/PlayerExperienceFactory.php
+++ database/factories/PlayerExperienceFactory.php
@@ -6,9 +6,9 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PlayerExperiences>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PlayerExperience>
  */
-class PlayerExperiencesFactory extends Factory
+class PlayerExperienceFactory extends Factory
 {
     /**
      * Define the model's default state.
