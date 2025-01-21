2cbaf78602c7b4a6c474f0b0ed044dec9ac9f218
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2025-01-21 10:57:31 +0100

    remove uncompromised validation for password

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -28,7 +28,7 @@ rules([
         'required',
         'string',
         'confirmed',
-        Rules\Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised()
+        Rules\Password::min(8)->mixedCase()->numbers()->symbols()
     ],
 ]);
 
